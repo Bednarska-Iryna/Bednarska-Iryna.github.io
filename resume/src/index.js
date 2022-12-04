081598c2f98f35c6e95ea2216f5a893543e9c7cb
Bedarska-Iryna <bednarska.ira.2000@gmail.com>
2022-12-04 14:59:57 +0200

    HashRouter

--- resume/src/index.js
+++ resume/src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 // import store from "./store"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      {/* <Provider store={store}> */}
-          <BrowserRouter>
-              <App/>
-          </BrowserRouter>
-      {/* </Provider> */}
+    {/* <Provider store={store}> */}
+    <HashRouter>
+      <App />
+    </HashRouter>
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
