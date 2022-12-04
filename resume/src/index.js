409433fd71b9f9ac32348ae5795fb523dde3f446
Bedarska-Iryna <bednarska.ira.2000@gmail.com>
2022-12-04 16:05:52 +0200

    NEW

--- resume/src/index.js
+++ resume/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
     </HashRouter>
     {/* </Provider> */}
